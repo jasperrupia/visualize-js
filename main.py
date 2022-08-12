06ff465d2fedbe3dc854e74a3b57d4f8a2bc7f9c
katunz jr <84384453+Jasper-Rupia@users.noreply.github.com>
2022-08-12 08:25:18 +0300

    .

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from flask import Flask , render_template
 app = Flask(__name__) #creating the Flask class object   
  
 
-@app.route('/') #decorator drfines the   
+@app.route('/chartjs') 
 def chartJs():  
-    return render_template('expertChart.html');  
+    return render_template('chartjs.html');  
+
+@app.route('/apexcharts') 
+def apexCharts():  
+    return render_template('apexcharts.html');  
 
 
 if __name__ =='__main__':  
